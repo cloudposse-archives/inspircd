83268b598f2c2fdaf5acbe881647691652f41fea
Erik Osterman <e@osterman.com>
2016-03-23 12:44:20 -0700

    add users to schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,29 @@
 --
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace:
+--
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nick` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `user_name` varchar(128) NOT NULL,
+  `group` varchar(100) DEFAULT NULL,
+  `registered_at` TIMESTAMP DEFAULT NULL,
+  `approved_at` TIMESTAMP DEFAULT NULL,
+  `disabled_at` TIMESTAMP DEFAULT NULL,
+  `created_at` TIMESTAMP DEFAULT now(),
+  `vhost` varchar(150) DEFAULT NULL,
+  `umode` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_user` (`nick`),
+  KEY `unique_user` (`nick`),
+  KEY `index_multi` (`nick`,`password`,`approved_at`,`disabled_at`) USING BTREE,
+  KEY `index_approved_at` (`approved_at`),
+  KEY `index_disabled_at` (`disabled_at`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='For InspIRCd SQL Auth';
+
+--
 -- Name: events; Type: TABLE; Schema: public; Owner: -; Tablespace:
 --
 
